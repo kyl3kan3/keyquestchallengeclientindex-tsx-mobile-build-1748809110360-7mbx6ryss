01d30c1ccc28aa7d806c5ab11f7c7292c47ddf4b
kyl3kan3 <kyl3kan3@gmail.com>
2025-06-01 15:18:35 -0500

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aiappbuilder.keyquestchallengeclientindextsxmobilebuild17488091103607mbx6ryss',
+  appName: 'KeyQuestChallengeclientindex.tsx',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
